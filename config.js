ddec04e17ac09463f606e705f43ae81740ea6e16
PROGRAMAR NIMAH <nimsaranimsara704@gmail.com>
2025-12-23 23:41:59 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "gnOgvgXh6MLDv7eQwJBEy1PORfir3q5DYbv2ys4mm8U",
+SESSION_ID: process.env.SESSION_ID || "NIMA~MD&gnOgvgXh6MLDv7eQwJBEy1PORfir3q5DYbv2ys4mm8U",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
